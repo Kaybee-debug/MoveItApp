18664f47c66b7c65e93a2f8ea77aaa210f8d6c48
kaybee-debug <karabomolepo29@gmail.com>
2023-03-20 14:24:17 +0200

    modifield urls

--- MoveItProject/urls.py
+++ MoveItProject/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('Accounts/', include("Accounts.urls") ),
     path('Drivers/', include("Driver.urls") ),
     path('Relocate/', include("Relocate.urls") ),
+<<<<<<< HEAD
+=======
+    path('Payment/', include("Payment.urls") ),
+>>>>>>> cc95097 (modified files)
 ]
