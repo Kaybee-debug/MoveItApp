587616de4d5ba4f4c146ce549c11029382e2b3bf
kaybee-debug <karabomolepo29@gmail.com>
2023-03-25 09:48:01 +0200

    modified files

--- MoveItProject/urls.py
+++ MoveItProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "MoveIT Admin"
+admin.site.index_title ="KelvinandKarabo Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Home.urls") ),
