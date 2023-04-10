b1d77f3158ce454ff7e4bd8046e5cdd4752a6f7d
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-04-10 17:31:31 +0200

    modified files

--- Driver/urls.py
+++ Driver/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
      path("display",views.displayDriver,name="display"),
      path('show/<int:relocate_id>/', views.displayVihicle, name='show'),
-     path('relocate/<int:relocate_id>/calculate_price/<int:distance>/', views.calculate_price, name='calculate_price'),
+     path('relocate/<int:relocate_id>/calculate_price/<int:distance>/', views.calculate_price, name='calculate_price', kwargs={'distance': 0}),
+
+     # path('relocate/<int:relocate_id>/calculate_price/<int:distance>/', views.calculate_price, name='calculate_price'),
      # path("detail",views.displayDetail,name="detail"),
 ]
 
